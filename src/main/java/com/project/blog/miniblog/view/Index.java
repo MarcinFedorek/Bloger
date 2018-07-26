7e0279c3ced8f0ad2a43bf603b6b1093e74fda29
Driver <peplinskikrzysztof@gmail.com>
2018-07-26 19:56:18 +0200

    manu bar in index

--- src/main/java/com/project/blog/miniblog/view/Index.java
+++ src/main/java/com/project/blog/miniblog/view/Index.java
@@ -2,23 +2,29 @@ package com.project.blog.miniblog.view;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Layout;
-import com.vaadin.ui.MenuBar;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 
-
-@SpringUI
+@SpringUI(path = "index")
 public class Index extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        HorizontalLayout menulayout = new HorizontalLayout();
+        menulayout.setMargin(true);
+        menulayout.setSpacing(true);
+
         HorizontalLayout layout = new HorizontalLayout();
+        layout.setSpacing(true);
+
+        Label indexLabel = new Label("Home");
+        Label usersLabel = new Label("Users List");
+        Label postLabel = new Label("Users List");
 
 
-
-       // layout.addComponent();
-        setContent(layout);
+        menulayout.addComponent(indexLabel);
+        menulayout.addComponent(usersLabel);
+        menulayout.addComponent(postLabel);
+        setContent(menulayout);
     }
 
 
