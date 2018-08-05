16e9b01e7ef0b1a76e18f3ede5cc90ce27aa881c
Driver <peplinskikrzysztof@gmail.com>
2018-08-05 17:44:46 +0200

    todo

--- src/main/java/com/project/blog/miniblog/controllers/LoginController.java
+++ src/main/java/com/project/blog/miniblog/controllers/LoginController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-
+//todo doczytać dlaczego spring 1.5.2 działa bez mapowania a wysypuje z mapowaniem
+//todo "pisze że jest już zaimplementowana klasa kontrolera"
     /*@RequestMapping(path = "/singup")
     public String getLogin(){
 
